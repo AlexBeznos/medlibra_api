8c83a4fb3d185e68cfa2bc80af51cdf5e30723de
alextrueman <alexandr1golubenko@gmail.com>
2020-10-03 23:45:10 +0300

    improve worker spec

--- spec/workers/first_worker_spec.rb
+++ spec/workers/first_worker_spec.rb
@@ -7,9 +7,16 @@ RSpec.describe Medlibra::Workers::FirstWorker do
   it "runs worker" do
     uid = SecureRandom.hex
     Factory[:user, uid: uid]
-    described_class.perform_async(uid)
 
-    expect(described_class).to have_enqueued_job(uid)
+    users_repo = instance_double(Medlibra::Repositories::UsersRepo)
+    Medlibra::Container.stub("repositories.users_repo", users_repo)
+
+    allow(users_repo).to receive(:by_uid).with(uid)
+
+    described_class.new.perform(uid)
+
+    expect(users_repo).to have_received(:by_uid).with(uid)
+    Medlibra::Container.unstub("repositories.users_repo")
   end
 
   it "runs worker in 10 minutes" do
